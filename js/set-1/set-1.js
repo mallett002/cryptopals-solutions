d81d4ff74a2145c6fd5a9a72069f4a3a550b8c82
William Mallett <mallett002@gmail.com>
2024-07-18 21:18:25 -0500

    c2: fixed xor

--- js/set-1/set-1.js
+++ js/set-1/set-1.js
@@ -2,6 +2,25 @@ function hexToBase64(hexStr) {
     return Buffer.from(hexStr, 'hex').toString('base64');
 }
 
+function xor(a, b) {
+    const length = Math.max(a.length, b.length);
+    const buffer = Buffer.allocUnsafe(length);
+
+    for (let i = 0; i < length; i++) {
+        buffer[i] = a[i] ^ b[i];
+    }
+
+    return buffer;
+}
+
+function generateXOR(source, comparator) {
+    const bufferOne = Buffer.from(source, 'hex');
+    const bufferTwo = Buffer.from(comparator, 'hex');
+
+    return xor(bufferOne, bufferTwo).toString('hex');
+}
+
 module.exports = {
     hexToBase64,
+    generateXOR,
 };
--- js/set-1/set-1.test.js
+++ js/set-1/set-1.test.js
@@ -1,9 +1,18 @@
-const { hexToBase64 } = require("./set-1");
+const { hexToBase64, generateXOR } = require("./set-1");
 
 describe('set-1', () => {
     test('C1: Convert hex to base64', () => {
         const result = hexToBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d');
 
         expect(result).toStrictEqual('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t');
+    });
+
+    test('C2: Fixed XOR', () => {
+        const source = '1c0111001f010100061a024b53535009181c';
+        const comparator = '686974207468652062756c6c277320657965';
+
+        const result = generateXOR(source, comparator);
+
+        expect(result).toStrictEqual('746865206b696420646f6e277420706c6179');
     });
 });
