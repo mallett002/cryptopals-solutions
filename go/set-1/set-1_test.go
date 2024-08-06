9266e103c1475672ba748f9ace896b9262b8c78a
William Mallett <mallett002@gmail.com>
2024-08-06 06:54:46 -0500

    setup for golang c5

--- go/set-1/set-1_test.go
+++ go/set-1/set-1_test.go
@@ -41,3 +41,13 @@ func TestFindEncryptionKeyInFile(t *testing.T) {
 	message := FindTextFromFileWithKey(fileName, 53);
 	assert.Equal(t, "Now that the party is jumping\n", message)
 }
+
+func TestRepeatingKeyXOR(t *testing.T) {
+	plainText := `Burning 'em, if you ain't quick and nimble 
+I go crazy when I hear a cymbal`;
+	expectedEncryptedText := `0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f`
+
+	encryptedText := RepeatingKeyXOR(plainText);
+	assert.Equal(t, expectedEncryptedText, encryptedText)
+}
