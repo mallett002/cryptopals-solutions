6d58ab00b397a2fd952acb1f6568a6d9de56a15c
William Mallett <mallett002@gmail.com>
2024-08-07 05:57:59 -0500

    wip on go c5

--- go/set-1/set-1_test.go
+++ go/set-1/set-1_test.go
@@ -43,11 +43,14 @@ func TestFindEncryptionKeyInFile(t *testing.T) {
 }
 
 func TestRepeatingKeyXOR(t *testing.T) {
+	// https://cryptopals.com/sets/1/challenges/5
+
 	plainText := `Burning 'em, if you ain't quick and nimble 
 I go crazy when I hear a cymbal`;
 	expectedEncryptedText := `0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
 a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f`
+	key := "ICE"
 
-	encryptedText := RepeatingKeyXOR(plainText);
+	encryptedText := RepeatingKeyXOR(plainText, key);
 	assert.Equal(t, expectedEncryptedText, encryptedText)
 }
