2a7501d41e1ff72ff7dfa8e77ae135bc50ea688b
William Mallett <mallett002@gmail.com>
2024-09-09 08:46:58 -0500

    starting c9

---
+++ js/set-2/c9.test.js
@@ -0,0 +1,16 @@
+describe('Set-2: "block cipher cryptography"', () => {
+    test('Implement PKCS#7 padding', () => {
+        /*
+            - Block cipher encrypts a fixed size block of plaintext (usually 8 to 16 bytes) into ciphertext
+            - We usually encryt irregular sizes, and not fixed size blocks though
+            - To fix this, we create regular sized blocks by adding padding, such as PKCS#7
+            - Pad blocks to specific block length by adding the number of desired bytes of padding
+                - Ex: "YELLOW SUBMARINE" padded to 20 bytes would be "YELLOW SUBMARINE\x04\x04\x04\x04"
+        */
+       const plainText = 'YELLOW SUBMARINE';
+
+       const withPadding = getPaddedPlainText(plainText, 20);
+       
+       expect(withPadding).toStrictEqual("YELLOW SUBMARINE\x04\x04\x04\x04");
+    });
+});
