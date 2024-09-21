e9ab2b68eaac241084006dc1a57302749a34e6ad
William Mallett <mallett002@gmail.com>
2024-09-21 10:41:38 -0500

    random int gen

--- go/set-2/c11/c11.go
+++ go/set-2/c11/c11.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"crypto/rand"
+	"math/big"
 )
 
-func GenerateRandomAESKey() []byte {
-	token := make([]byte, 16)
+func GenerateRandomAESKey(byteLength int) []byte {
+	token := make([]byte, byteLength)
 
-	return rand.Reader.Read(token)
+	_, err := rand.Read(token)
+	if err != nil {
+		log.Fatalf("error generating random key: %v", err)
+	}
+
+	return token
 }
 
-// 
-func EncryptionOracle(input []byte) []byte {
+func GenerateRandomInt() int {
+	min := big.NewInt(5)
+	max := big.NewInt(10)
+	diff := big.NewInt(0).Sub(max, min)
+	diffPlusOne := big.NewInt(0).Add(diff, big.NewInt(1))
+
+	nBig, err := rand.Int(rand.Reader, diffPlusOne)
+	if err != nil {
+		log.Fatalf("error generating random number: %v", err)
+	}
+
+	n := big.NewInt(0).Add(nBig, min).Int64()
+
+	return int(n)
+}
+
+// Appends 5-10 random bytes before plaintext and 5-10 bytes after plaintext
+// Encrypts ECB 1/2 the time and CBC other half - rand(2) each time to decide
+// 	- uses random IVs each time for CBC
+// Detects which mode (ECB || CBC) used
+func EncryptionOracle(plaintext []byte) []byte {
+	randomKey := GenerateRandomAESKey(16)	
+
+	fmt.Printf("random key: %v\n", randomKey)
+	fmt.Printf("random int: %v\n", GenerateRandomInt())
+
 
 	return []byte("hi")
 }
--- go/set-2/c11/c11_test.go
+++ go/set-2/c11/c11_test.go
@@ -7,11 +7,10 @@ import (
 
 func TestECBAndCBCDetectionOracle(t *testing.T) {
 	// Write function to generate random AES key (16 random bytes)
-	randomKey := GenerateRandomAESKey()
-	fmt.Println(string(randomKey))
+	randomKey := GenerateRandomAESKey(16)
+	fmt.Println(len(randomKey))
+
 	// Write function that uses this random key generation and encrypts data with it
-
-
 	input := "In case I don't see ya, good morning, good afternoon, and good night!"
 
 	cipherText := EncryptionOracle([]byte(input))
